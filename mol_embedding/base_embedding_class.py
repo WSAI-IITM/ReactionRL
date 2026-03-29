41537ea3b3ac0b5ed32230c96281de132b01dcb9
dumdum <dumdum@percival.infocusp.com>
2026-03-29 22:22:24 +0530

    Restructured and modularized for ease of reproduction.

--- mol_embedding/base_embedding_class.py
+++
@@ -1,8 +0,0 @@
-class BaseEmbeddingClass:
-    def mol_to_embedding(self):
-        # return the animal's age
-        raise NotImplementedError()
-
-    def atom_to_embedding(self):
-        # check if animal is dead or alive
-        raise NotImplementedError()
